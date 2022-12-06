e5975360c4e152b4a7400c50d98d95f42c1db74d
Shakhnavaz <shakhnavaz@gmail.com>
2022-12-06 16:24:29 +0300

    complete 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = ''
+    const error = ('ERROR')
+    if (num >= 0) {
+        for (let i = 1; i <= num; i++) {
+            result += string
+        }
+    } else {
+        return error
+    }
+    return result
 };
+
+repeatString('hey', 1)
 
 // Do not edit below this line
 module.exports = repeatString;
