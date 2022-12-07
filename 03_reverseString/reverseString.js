b9542fa20ff6a0192e982223f73f5f93e5f6bcfc
Shakhnavaz <shakhnavaz@gmail.com>
2022-12-07 18:41:14 +0300

    task number 3 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result
+    splitting = string.split("")
+    reversing = splitting.reverse()
+    joining = splitting.join("")
+    return joining
 };
+
+reverseString('hello')
 
 // Do not edit below this line
 module.exports = reverseString;
